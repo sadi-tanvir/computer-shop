60e0676d72cb3e46248fddd441643135e26081d9
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-09 02:23:38 +0600

    fixed 404 page responsive issue

--- src/components/404/Error404.js
+++ src/components/404/Error404.js
@@ -4,8 +4,8 @@ const Error404 = () => {
     return (
         <>
             <div className="w-full mt-20">
-                <h1 className="text-9xl font-bold text-center text-cyan-900">Error 404!</h1>
-                <p className="text-3xl font-semibold text-slate-500 text-center mt-2">The page you requested does not exist or <br /> has moved.</p>
+                <h1 className="text-6xl md:text-9xl font-bold text-center text-cyan-900">Error 404!</h1>
+                <p className="text-lg md:text-3xl font-semibold text-slate-500 text-center mt-2">The page you requested does not exist or <br /> has moved.</p>
             </div>
         </>
     );
