6f3dd80c2c13285665995c6767eda9af1b9ea068
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-09 00:47:21 +0600

    about componenet re-upload

---
+++ src/components/About/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+const About = () => {
+  return (
+    <>
+      <div className="w-full flex justify-center items-center mt-10">
+        <div className="w-6/12 shadow-lg hover:shadow-xl flex px-4 py-10">
+          <div className="flex flex-col justify-center items-center">
+            <img className="w-36 rounded-lg" src="/img/profile.jpg" alt="" />
+            <div className="mt-5">
+              <a href="https://web.facebook.com/" target="_blank">
+                <i class="fa-brands fa-facebook text-blue-500 cursor-pointer"></i>
+              </a>
+              <a href="https://twitter.com/" target="_blank">
+                <i class="fa-brands fa-twitter text-blue-900 ml-2 cursor-pointer"></i>
+              </a>
+              <a href="https://www.instagram.com/" target="_blank">
+                <i class="fa-brands fa-instagram text-red-400 ml-2 cursor-pointer"></i>
+              </a>
+            </div>
+          </div>
+          <div className="ml-4">
+            <h3 className="text-2xl font-semibold">Tanvir Hossain Sadi</h3>
+            <small className="">Web developer</small>
+            <hr className="mt-5 mb-2" />
+            <p className="text-base text-slate-800">Email: sadi@gmail.com</p>
+            <p className="text-base text-slate-800">Phone: 018******22</p>
+            <p className="text-base text-slate-800">city: Dhaka</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
